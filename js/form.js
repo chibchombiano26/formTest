6f618deae2b8621d4c741290156f574efbc78758
Jose douglas ramirez <chibchombiano26@hotmail.com>
2016-06-09 19:32:30 +0000

    Initial

---
+++ js/form.js
@@ -0,0 +1,37 @@
+/*global angular*/
+angular.module('myModule', ['schemaForm', 'mgcrea.ngStrap'])
+       .controller('FormController', function($scope) {
+  $scope.schema = {
+    type: "object",
+    properties: {
+      firstName: { type: "string", minLength: 2, title: "First Name" },
+      lastName: { type: "string", minLength: 2, title: "Last Name" },
+      email: { type: "string", minLength: 2, title: "Email", 'pattern': '^\\S+@\\S+$'},
+      date:{
+        title: "Date of birth",
+        type: "string",
+        format: "date"
+      }
+    }
+  };
+
+  $scope.form = [
+    "*",
+    {
+   "key": 'multiselect',
+   "type": 'strapselect',
+   "options": { 
+    "multiple": "true"
+   },
+   "titleMap": [
+        {"value": 'value1', "name": 'text1'},
+        {"value": 'value2', "name": 'text2'},
+        {"value": 'value3', "name": 'long very very long label3'}
+   ]
+ },
+    { type: 'submit', title: 'Cancel', onClick: function(){ console.log($scope.model);  } },
+    { type: 'button', style: 'btn-danger', title: 'Cancel', onClick: function(){ console.log($scope.model);  } }
+  ];
+
+  $scope.model = {};
+});
